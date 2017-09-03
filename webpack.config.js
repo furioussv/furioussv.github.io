5bbec55d5ed7120ea00af2810b98e3893393fcc0
furioussv <furioussv@gmail.com>
2017-09-03 14:36:16 +0700

    html + portrait

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const stylus = require('./webpack/stylus');
+const extractCSS = require('./webpack/css.extract');
+const devserver = require('./webpack/devserver');
+const webpack = require('webpack');
+
+const PATHS = {
+    source: path.join(__dirname, 'cv_src'),
+    build: path.join(__dirname, 'cv')
+};
+
+const common = merge ([
+    {
+        entry: PATHS.source + '/index.js',
+        output: {
+            path: PATHS.build,
+            filename: '[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: PATHS.source + '/index.pug',
+            })
+        ],
+    },
+    pug()
+]);
+
+module.exports = function(env) {
+    if (env === 'production') {
+        return merge([
+            common,
+            extractCSS()
+        ]);
+    }
+    if (env === 'development') {
+        return merge([
+            common,
+            devserver(),
+            stylus()
+        ]);
+    }
+};
